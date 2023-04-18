70de8c764c2a975311b366e94ba9974b8b48f32b
Micah Fang <Micah.fang@outlook.com>
2023-04-18 17:12:38 +0800

    feat(*): Update vite config

--- example/vite/vite.config.js
+++ example/vite/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'example-demo',
   plugins: [react()],
+  base: 'twc-animate',
   build: {
     outDir: '../../dist',
   },
